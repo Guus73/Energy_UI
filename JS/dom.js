eacdcba27cc89c265258f84336f62b8c32be5079
Guus73 <guus@eldering.net>
2026-03-06 20:48:35 +0100

    Delete JS directory

--- JS/dom.js
+++
@@ -1,4 +0,0 @@
-export const el = Object.fromEntries([
-"consFile",
-"runSim"
-].map(id => [id, document.getElementById(id)]))
--- JS/utils.js
+++
@@ -1,7 +0,0 @@
-export function clamp(x,a,b){
-return Math.max(a,Math.min(b,x))
-}
-
-export function setProgress(pct){
-console.log("Progress:",pct)
-}
